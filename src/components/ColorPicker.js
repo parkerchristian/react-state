96c1481cf35671181cbc99d9c71c5c46c62e2220
Parker Helland <hellandp@gmail.com>
2019-05-14 11:21:17 -0700

    colorpicker.js & css

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react';
+import styles from './ColorPicker.css';
+
+export default class ColorPicker extends PureComponent {
+  state = {
+    color: 'black'
+  };
+
+  colorClicked = color => {
+    this.setState({ color });
+    console.log(`click ${color}`);
+  }
+  
+  render() {
+    const { color } = this.state;
+
+    return (
+      <section className={styles.ColorPicker}>
+        <button className={styles.red} onClick={this.colorClicked.bind(null, 'red')}>RED</button>
+        <button className={styles.yellow} onClick={() => this.colorClicked('yellow')}>YELLOW</button>
+        <button className={styles.blue} onClick={() => this.colorClicked('blue')}>BLUE</button>
+        <div className={styles[color]}></div>
+      </section>
+    );
+  }
+}
